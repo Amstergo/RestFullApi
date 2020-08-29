06ebdff0e27612e77e21bdf2d31b37f463d6edec
Amstergo <Amstergo46@gmail.com>
2020-08-29 12:44:16 +0300

    added edit activities

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -37,5 +37,12 @@ namespace API.Controllers
         {
             return await _mediator.Send(command);
         }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult<Unit>> Edit(Edit.Command command, int id)
+        {
+            command.Id = id;
+            return await _mediator.Send(command);
+        }
     }
 }
